01bb6a4ed774a1565e5f11e844d8539550819e72
Avrumi Friedman <alex+5@lakewoodprogramming.com>
2018-06-13 22:20:03 -0400

    Added cw

--- HelloGit/Global.asax.cs
+++ HelloGit/Global.asax.cs
@@ -16,6 +16,7 @@ namespace HelloGit
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Console.WriteLine("yay!");
         }
 
         private void foo()
